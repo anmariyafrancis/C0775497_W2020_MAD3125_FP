835f5ffa32acec632f2c66964134585e5802b3b5
anmariya <anmariya1915@gmail.com>
2020-04-16 20:20:50 -0400

    [ANMARIYA]declared variables used in login

--- app/src/main/java/com/example/c0775497_w2020_mad3125_fp/LoginActivity.java
+++ app/src/main/java/com/example/c0775497_w2020_mad3125_fp/LoginActivity.java
@@ -2,9 +2,22 @@ package com.example.c0775497_w2020_mad3125_fp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private String emailId,password;
+    private String login;
+    private EditText txtEmail,txtPassword;
+    private CheckBox CheckRemember;
+    private Boolean rememberMe;
+    private SharedPreferences loginPreferences;
+    private SharedPreferences.Editor loginPrefEditor;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
