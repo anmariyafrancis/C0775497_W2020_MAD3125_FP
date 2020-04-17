48924991a113d5299cf419585939cc36c459a869
anmariya <anmariya1915@gmail.com>
2020-04-17 19:59:15 -0400

    [ANMARIYA]created array to store arraylist

--- app/src/main/java/com/example/c0775497_w2020_mad3125_fp/CustomerListActivity.java
+++ app/src/main/java/com/example/c0775497_w2020_mad3125_fp/CustomerListActivity.java
@@ -1,10 +1,19 @@
 package com.example.c0775497_w2020_mad3125_fp;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class CustomerListActivity extends AppCompatActivity {
+
+    private RecyclerView rvCustomerList;
+    private ArrayList customers;
+    private ArrayList customersList;
+    private CustomerAdapter customerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
