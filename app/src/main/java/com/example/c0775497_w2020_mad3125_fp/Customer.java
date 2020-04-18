bf9709c0be0e02f8bed169c228861fb0317b985a
anmariya <anmariya1915@gmail.com>
2020-04-17 20:16:02 -0400

    [ANMARIYA]created varibles in customer class

--- app/src/main/java/com/example/c0775497_w2020_mad3125_fp/Customer.java
+++ app/src/main/java/com/example/c0775497_w2020_mad3125_fp/Customer.java
@@ -1,4 +1,19 @@
 package com.example.c0775497_w2020_mad3125_fp;
 
+import java.util.HashMap;
+
 public class Customer {
+
+    private String customerId;
+    private String firstName;
+    private String lastName;
+    private String gender;
+    private String email;
+    private String userName;
+    private String password;
+    private String address;
+    private String birthDate;
+    private Double fullTotal;
+    private int customerImage;
+
 }
--- app/src/main/java/com/example/c0775497_w2020_mad3125_fp/CustomerAdapter.java
+++ app/src/main/java/com/example/c0775497_w2020_mad3125_fp/CustomerAdapter.java
@@ -1,4 +1,10 @@
 package com.example.c0775497_w2020_mad3125_fp;
 
+import java.util.ArrayList;
+
 public class CustomerAdapter {
+    ArrayList<Customer>customerArrayList;
+    public CustomerAdapter(ArrayList<Customer> cu) {
+        this.customerArrayList=cu;
+    }
 }
