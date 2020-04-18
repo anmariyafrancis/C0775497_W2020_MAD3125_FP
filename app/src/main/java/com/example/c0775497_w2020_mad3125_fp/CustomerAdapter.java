63ede96526bfa57a7d8ff7fedf853526ac10f804
anmariya <anmariya1915@gmail.com>
2020-04-17 20:22:17 -0400

    [ANMARIYA]extended recyclerview.adapter

--- app/src/main/java/com/example/c0775497_w2020_mad3125_fp/CustomerAdapter.java
+++ app/src/main/java/com/example/c0775497_w2020_mad3125_fp/CustomerAdapter.java
@@ -1,10 +1,31 @@
 package com.example.c0775497_w2020_mad3125_fp;
 
+import android.view.ViewGroup;
+
 import java.util.ArrayList;
 
-public class CustomerAdapter {
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class CustomerAdapter extends RecyclerView.Adapter {
     ArrayList<Customer>customerArrayList;
     public CustomerAdapter(ArrayList<Customer> cu) {
         this.customerArrayList=cu;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
     }
 }
