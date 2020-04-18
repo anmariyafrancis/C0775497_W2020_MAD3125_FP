6e97829f3c5a0343bce6f584e6e9fdeed8b77214
anmariya <anmariya1915@gmail.com>
2020-04-17 21:32:56 -0400

    [ANMARIYA]created populatecustomers function

--- app/src/main/java/com/example/c0775497_w2020_mad3125_fp/CustomerListActivity.java
+++ app/src/main/java/com/example/c0775497_w2020_mad3125_fp/CustomerListActivity.java
@@ -10,6 +10,7 @@ import android.view.Menu;
 import android.view.MenuInflater;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 
 public class CustomerListActivity extends AppCompatActivity {
@@ -23,27 +24,30 @@ public class CustomerListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_list);
-        ActionBar mActionBar=getSupportActionBar();
+        ActionBar mActionBar = getSupportActionBar();
         mActionBar.setTitle("Customers List");
-        rvCustomerList=findViewById(R.id.rvCustomerList);
+        rvCustomerList = findViewById(R.id.rvCustomerList);
         populateCustomers();
 
-        customerAdapter=new CustomerAdapter(customers);
-        RecyclerView.LayoutManager mLinearLayoutManager=new LinearLayoutManager(this,RecyclerView.VERTICAL,false);
+        customerAdapter = new CustomerAdapter(customers);
+        RecyclerView.LayoutManager mLinearLayoutManager = new LinearLayoutManager(this, RecyclerView.VERTICAL, false);
         rvCustomerList.setLayoutManager(mLinearLayoutManager);
         rvCustomerList.setAdapter(customerAdapter);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu){
-        MenuInflater inflater=getMenuInflater();
-        inflater.inflate(R.menu.mymenu,menu);
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.mymenu, menu);
         return true;
     }
+
     private void populateCustomers() {
         DataSingelton.getInstance().populateData();
-        customers=new ArrayList();
-        HashMap<String,Customer> customerHashMap=Data
-
+        customers = new ArrayList();
+        HashMap<String, Customer> customerHashMap = DataSingelton.getInstance().getCustomerMap();
+        Collection<Customer> demoValues = customerHashMap.values();
+        customersList = new ArrayList<>(demoValues);
+        customers.addAll(customersList);
     }
 }
