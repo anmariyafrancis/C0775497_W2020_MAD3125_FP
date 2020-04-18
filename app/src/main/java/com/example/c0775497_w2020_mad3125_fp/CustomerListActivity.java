c62c1336c825cfba3f744f419ee7776595445af6
anmariya <anmariya1915@gmail.com>
2020-04-17 20:05:52 -0400

    [ANMARIYA]created Customer class

--- app/src/main/java/com/example/c0775497_w2020_mad3125_fp/CustomerListActivity.java
+++ app/src/main/java/com/example/c0775497_w2020_mad3125_fp/CustomerListActivity.java
@@ -2,6 +2,7 @@ package com.example.c0775497_w2020_mad3125_fp;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
@@ -19,5 +20,17 @@ public class CustomerListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_list);
+        ActionBar mActionBar=getSupportActionBar();
+        mActionBar.setTitle("Customers List");
+        rvCustomerList=findViewById(R.id.rvCustomerList);
+        populateCustomers();
+
+        customerAdapter=new CustomerAdapter(customers);
+        RecyclerView.LayoutManager mLinearLayoutManager=new LinearLayoutManager(this,RecyclerView.VERTICAL,false);
+        rvCustomerList.setLayoutManager(mLinearLayoutManager);
+        rvCustomerList.setAdapter(customerAdapter);
+    }
+
+    private void populateCustomers() {
     }
 }
