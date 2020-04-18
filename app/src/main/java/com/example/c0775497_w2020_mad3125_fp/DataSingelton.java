72561730dffdb46dccb0bea6ea84c400ed3fa587
anmariya <anmariya1915@gmail.com>
2020-04-17 21:25:36 -0400

    [ANMARIYA]created customer hashmap on datasingleton

--- app/src/main/java/com/example/c0775497_w2020_mad3125_fp/DataSingelton.java
+++ app/src/main/java/com/example/c0775497_w2020_mad3125_fp/DataSingelton.java
@@ -1,4 +1,14 @@
 package com.example.c0775497_w2020_mad3125_fp;
 
+import java.util.HashMap;
+
 public class DataSingelton {
+    private static DataSingelton repoObj=new DataSingelton();
+    public static DataSingelton getInstance(){return repoObj;}
+    private DataSingelton(){}
+    private HashMap<String,Customer>customerMap=new HashMap<>();
+    public HashMap getCustomerMap(){return this.customerMap;}
+    public void populateData(){
+        Customer c1=new Customer();
+    }
 }
