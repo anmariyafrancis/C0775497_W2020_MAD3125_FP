4ee32c5a8ec2d9988872c9a6fa497a2ff9ac48ca
anmariya <anmariya1915@gmail.com>
2020-04-20 14:12:59 -0400

    [ANMARIYA]generated getter and setter for customer class

--- app/src/main/java/com/example/c0775497_w2020_mad3125_fp/Customer.java
+++ app/src/main/java/com/example/c0775497_w2020_mad3125_fp/Customer.java
@@ -50,7 +50,105 @@ public class Customer implements Parcelable {
     this.customerId=customerId;
     this.firstName=firstName;
     this.lastName=lastName;
+        this.email=email;
+        this.userName=userName;
+        this.password=password;
+        this.gender=gender;
+        this.address=address;
+        this.birthDate=birthDate;
+        this.customerImage=customerImage;
+    }
 
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(String birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public Double getFullTotal() {
+        return fullTotal;
+    }
+
+    public void setFullTotal(Double fullTotal) {
+        this.fullTotal = fullTotal;
+    }
+
+    public void setCustomerImage(int customerImage) {
+        this.customerImage = customerImage;
+    }
+
+    public HashMap<String, Bill> getCustomerBills() {
+        return customerBills;
+    }
+
+    public void setCustomerBills(HashMap<String, Bill> customerBills) {
+        this.customerBills = customerBills;
     }
 }
 
