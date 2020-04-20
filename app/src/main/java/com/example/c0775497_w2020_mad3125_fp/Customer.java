74c75991558e6df69e52792f30564f45745be821
anmariya <anmariya1915@gmail.com>
2020-04-20 13:53:59 -0400

    [ANMARIYA]created array for customer

--- app/src/main/java/com/example/c0775497_w2020_mad3125_fp/Customer.java
+++ app/src/main/java/com/example/c0775497_w2020_mad3125_fp/Customer.java
@@ -1,11 +1,13 @@
 package com.example.c0775497_w2020_mad3125_fp;
 
 import android.os.Parcel;
+import android.os.Parcelable;
 
+import java.security.PublicKey;
 import java.util.BitSet;
 import java.util.HashMap;
 
-public class Customer {
+public class Customer implements Parcelable {
 
     private String customerId;
     private String firstName;
@@ -18,20 +20,30 @@ public class Customer {
     private String birthDate;
     private Double fullTotal;
     private int customerImage;
-    private HashMap<String, Bill> customerBills=new HashMap<String, Bill>();
+    private HashMap<String, Bill> customerBills = new HashMap<String, Bill>();
 
-    protected Customer(Parcel in){
-        customerId= in.readString();
-        firstName= in.readString();
-        lastName= in.readString();
-        gender= in.readString();
-        email= in.readString();
-        userName= in.readString();
-        password= in.readString();
-        address= in.readString();
-        fullTotal= in.readDouble();
-        birthDate= in.readString();
-        customerImage=in.readInt();
-        customerBills=in.readHashMap(Bill.class.getClassLoader());
+    protected Customer(Parcel in) {
+        customerId = in.readString();
+        firstName = in.readString();
+        lastName = in.readString();
+        gender = in.readString();
+        email = in.readString();
+        userName = in.readString();
+        password = in.readString();
+        address = in.readString();
+        fullTotal = in.readDouble();
+        birthDate = in.readString();
+        customerImage = in.readInt();
+        customerBills = in.readHashMap(Bill.class.getClassLoader());
     }
+
+    public static final Creator<Customer> CREATOR = new Creator<Customer>() {
+        @Override
+        public Customer createFromParcel(Parcel in) {
+            return new Customer(in);
+        }
+        @Override
+        public Customer[] newArray(int size){return new Customer[size];}
+    };
 }
+
