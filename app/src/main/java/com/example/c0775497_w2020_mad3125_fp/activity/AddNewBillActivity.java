cd4ece847cc625668bd16606d620152c3b150c3f
anmariya <anmariya1915@gmail.com>
2020-04-20 16:56:37 -0400

    [ANMARIYA]created add new bill

---
+++ app/src/main/java/com/example/c0775497_w2020_mad3125_fp/activity/AddNewBillActivity.java
@@ -0,0 +1,14 @@
+package com.example.c0775497_w2020_mad3125_fp.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddNewBillActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_new_bill);
+    }
+}
