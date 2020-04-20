099b5de07356979c61ee7d86d0fb5e28a5557f2d
anmariya <anmariya1915@gmail.com>
2020-04-20 11:59:38 -0400

    [ANMARIYA]variables created for each field in add new customer

--- app/src/main/java/com/example/c0775497_w2020_mad3125_fp/AddNewCustomer.java
+++ app/src/main/java/com/example/c0775497_w2020_mad3125_fp/AddNewCustomer.java
@@ -3,9 +3,16 @@ package com.example.c0775497_w2020_mad3125_fp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
 
 public class AddNewCustomer extends AppCompatActivity {
 
+    private EditText edtxtId,edtxtFirstName,edtxtLastName,edtxtBirth;
+    private RadioButton rdBtnMale,rdBtnFemale,rdBtnOther;
+    private EditText edtxtEmail,edtxtUserName,edtxtPassword,edtxtAdress;
+    private Button btnAdd,btnClear;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
