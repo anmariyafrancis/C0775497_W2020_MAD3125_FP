a9190a6c2a6342a9981c827e9b44f59ca100244d
anmariya <anmariya1915@gmail.com>
2020-04-20 19:01:24 -0400

    [ANMARIYA]screenshot added

--- app/src/main/java/com/example/c0775497_w2020_mad3125_fp/activity/CustomerListActivity.java
+++ app/src/main/java/com/example/c0775497_w2020_mad3125_fp/activity/CustomerListActivity.java
@@ -5,9 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.example.c0775497_w2020_mad3125_fp.model.Customer;
 import com.example.c0775497_w2020_mad3125_fp.adapter.CustomerAdapter;
@@ -44,6 +46,21 @@ public class CustomerListActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.mymenu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.btnLogout:
+                Intent logout = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(logout);
+                finish();
+                return true;
+            case R.id.btnAdd:
+                startActivity(new Intent(CustomerListActivity.this, AddNewCustomerActivity.class));
+        }
         return true;
     }
 
